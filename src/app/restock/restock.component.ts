3877adea202d9a27d059a613bc8cf8503c716526
Mike Wester <mpwester@gmail.com>
2020-09-06 11:12:33 -0500

    Restock

--- src/app/restock/restock.component.ts
+++ src/app/restock/restock.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AtmHistoryService } from '../../services/atm-history/atm-history.service';
+import { AtmStateService } from '../../services/atm-state/atm-state.service';
+import { CurrencyValue } from '../../enum/currency-value.enum';
 
 @Component({
   selector: 'app-restock',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestockComponent implements OnInit {
 
-  constructor() { }
+  public restockForm: FormGroup;
+  public restockSuccessFull: boolean = false;
+
+  constructor(
+    private atmHistoryService: AtmHistoryService,
+    public atmStateService: AtmStateService
+  ) { 
+    this.restockForm = new FormGroup({
+      amountHundread: new FormControl(0, Validators.min(0)),
+      amountFifty: new FormControl(0, Validators.min(0)),
+      amountTwenty: new FormControl(0, Validators.min(0)),
+      amountFive: new FormControl(0, Validators.min(0)),
+      amountOne: new FormControl(0, Validators.min(0))
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  restock(): void {
+    this.atmStateService.addStock(CurrencyValue.hundread, this.restockForm.controls['amountHundread'].value);
+    this.atmStateService.addStock(CurrencyValue.fifty, this.restockForm.controls['amountFifty'].value);
+    this.atmStateService.addStock(CurrencyValue.twenty, this.restockForm.controls['amountTwenty'].value);
+    this.atmStateService.addStock(CurrencyValue.five, this.restockForm.controls['amountFive'].value);
+    this.atmStateService.addStock(CurrencyValue.dollar, this.restockForm.controls['amountOne'].value);
+  }
 }
--- src/services/atm-state/atm-state.service.ts
+++ src/services/atm-state/atm-state.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Currency } from '../../interface/currency.interface';
 import { CurrencyValue } from '../../enum/currency-value.enum';
 
@@ -7,15 +8,26 @@ import { CurrencyValue } from '../../enum/currency-value.enum';
 })
 export class AtmStateService {
 
-  private _currentStock: Currency[] = [
-    { value: CurrencyValue[CurrencyValue.hundread], display: 100, amount: 10 },
-    { value: CurrencyValue[CurrencyValue.fifty], display: 50, amount: 10 },
-    { value: CurrencyValue[CurrencyValue.twenty], display: 20, amount: 10 },
-    { value: CurrencyValue[CurrencyValue.five], display: 5, amount: 10 },
-    { value: CurrencyValue[CurrencyValue.dollar], display: 1, amount: 10 }
-  ];
+  private _currentStock: Currency[];
+  private _behaviorSubject: BehaviorSubject<Currency[]> = new BehaviorSubject(null);
 
-  public get currentStock(): Currency[] { return this._currentStock; } 
+  constructor() { 
+    this._currentStock = [
+      { value: CurrencyValue[CurrencyValue.hundread], display: 100, amount: 10 },
+      { value: CurrencyValue[CurrencyValue.fifty], display: 50, amount: 10 },
+      { value: CurrencyValue[CurrencyValue.twenty], display: 20, amount: 10 },
+      { value: CurrencyValue[CurrencyValue.five], display: 5, amount: 10 },
+      { value: CurrencyValue[CurrencyValue.dollar], display: 1, amount: 10 }
+    ];
 
-  constructor() { }
+    this._behaviorSubject.next(this._currentStock);
+  }
+
+  public getCurrentStock(): Observable<Currency[]> { return this._behaviorSubject.asObservable(); } 
+
+  public addStock(currencyValue: CurrencyValue, amount: number): boolean {
+    this._currentStock[currencyValue].amount += amount;
+    this._behaviorSubject.next(this._currentStock);
+    return true;
+  }
 }
