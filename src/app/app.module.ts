89228598c2649741251f149496499e680e656edc
Mike Wester <mpwester@gmail.com>
2020-11-26 12:08:20 -0600

    Fix OCDness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './shared/dashboard/dashboard.component';
-import { WithdrawComponent } from './user/withdraw/withdraw.component';
-import { RestockComponent } from './admin/restock/restock.component';
-import { OverviewComponent } from './admin/overview/overview.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { LoginComponent } from './login/login/login.component';
-import { UserLandingComponent } from './user/user-landing/user-landing.component';
 import { AdminLandingComponent } from './admin/admin-landing/admin-landing.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './login/login/login.component';
+import { OverviewComponent } from './admin/overview/overview.component';
+import { RestockComponent } from './admin/restock/restock.component';
 import { TransactionHistoryComponent } from './admin/transaction-history/transaction-history.component';
+import { UserLandingComponent } from './user/user-landing/user-landing.component';
+import { WithdrawComponent } from './user/withdraw/withdraw.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    WithdrawComponent,
-    RestockComponent,
-    OverviewComponent,
-    HeaderComponent,
-    FooterComponent,
-    LoginComponent,
-    UserLandingComponent,
     AdminLandingComponent,
-    TransactionHistoryComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    OverviewComponent,
+    RestockComponent,
+    TransactionHistoryComponent,
+    UserLandingComponent,
+    WithdrawComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     FormsModule,
     ReactiveFormsModule
   ],
