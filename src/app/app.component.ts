57c4780d8e40842e84df15b3478391b1234721de
Mike Wester <mpwester@gmail.com>
2020-11-27 17:47:39 -0600

    Attempting to make NPM package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-angular-atm',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-atm';
+  public title: string = 'Angular Atm';
 }
