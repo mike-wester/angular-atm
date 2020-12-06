bb96aad8db43dfee0fa9f3d4cdd48f660fad7324
Mike Wester <mpwester@gmail.com>
2020-12-06 11:21:10 -0600

    Attempting to add Child Routes

---
+++ src/app/wwwroot/user/user-balance/user-balance.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserBalanceComponent } from './user-balance.component';
+
+describe('UserBalanceComponent', () => {
+  let component: UserBalanceComponent;
+  let fixture: ComponentFixture<UserBalanceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserBalanceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserBalanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
