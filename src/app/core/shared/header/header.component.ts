a4dda38df4096df342ff5c0baef8b8b1bcf73da9
Mike Wester <mpwester@gmail.com>
2021-01-02 20:24:22 -0600

    Add logo routing

--- src/app/core/shared/header/header.component.ts
+++ src/app/core/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserType } from 'src/app/enum/index.enum';
 import { UserStateService } from 'src/app/services/user-state/user-state.service';
 
 @Component({
@@ -19,6 +20,22 @@ export class HeaderComponent implements OnInit {
     public logoutUser(): void {
         if (this._userStateService.processLogout()) {
             this._router.navigate(['login']);
+        }
+    }
+
+    public logoUser(): void {
+        switch (this._userStateService.getCurrentUser()?.userType) {
+            case UserType.basic:
+                this._router.navigate(['user-landing']);
+                break;
+            case UserType.admin:
+                this._router.navigate(['admin-landing']);
+                break;
+            case UserType.super:
+                this._router.navigate(['super-landing']);
+                break;
+            default:
+                this._router.navigate(['login']);
         }
     }
 
